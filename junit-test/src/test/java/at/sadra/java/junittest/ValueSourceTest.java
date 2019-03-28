5d4addc114e3aa395ac25847d1aa52c1beebb5d4
Sadra <31988724+xsadra@users.noreply.github.com>
2019-03-28 17:22:39 +0100

    Update ValueSource Annotation: Add conversion

--- junit-test/src/test/java/at/sadra/java/junittest/ValueSourceTest.java
+++ junit-test/src/test/java/at/sadra/java/junittest/ValueSourceTest.java
@@ -1,13 +1,16 @@
 package at.sadra.java.junittest;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @DisplayName("Test of ValueSource Annotation in @ParameterizedTest")
@@ -31,4 +34,43 @@ class ValueSourceTest {
         //          name);
         assertNotNull(name);
     }
+
+    @DisplayName("Convert Value Source test: String to Boolean")
+    @ParameterizedTest
+    @ValueSource(strings = {"true", "true"})
+    void booleanConversionValueSourceTest(Boolean parameter) {
+        assertTrue(parameter);
+    }
+
+    @DisplayName("Convert Value Source test: String to Integer")
+    @ParameterizedTest(name = "Number is {0}")
+    @ValueSource(strings = {"28", "18", "33", "6"})
+    void integerConversionValueSourceTest(Integer number) {
+        assertTrue(number > 5);
+    }
+
+    @DisplayName("Convert Value Source test: String to Double")
+    @ParameterizedTest(name = "Number is {0}")
+    @ValueSource(strings = {"28.5", "18.0", "33.1", "6D"})
+    void doubleConversionValueSourceTest(Double number) {
+        assertTrue(number > 5);
+    }
+
+    @DisplayName("Convert Value Source test: String to Enum")
+    @ParameterizedTest(name = "Month is {0}")
+    @ValueSource(strings = {"FEBRUARY", "MAY", "APRIL", "MARCH"})
+    void enumConversionValueSourceTest(Month month) {
+        assertNotNull(month);
+    }
+
+    @Disabled("LocalDate Conn't Convert String to Date!")
+    @DisplayName("Convert Value Source test: String to LocalDate")
+    @ParameterizedTest(name = "Month is {0}")
+    @ValueSource(strings = {"1990-2-3", "2001-5-6", "1985-4-9", "2013-3-1"})
+    void enumConversionValueSourceTest(LocalDate localDate) {
+        assertNotNull(localDate);
+    }
+
+
+
 }

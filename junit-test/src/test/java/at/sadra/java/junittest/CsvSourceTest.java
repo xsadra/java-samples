6da1b06d67054c21eb0a9570beec1fbda3bf3498
Sadra <31988724+xsadra@users.noreply.github.com>
2019-03-28 16:49:34 +0100

    Test of CsvSource Annotation in @ParameterizedTest

---
+++ junit-test/src/test/java/at/sadra/java/junittest/CsvSourceTest.java
@@ -0,0 +1,27 @@
+package at.sadra.java.junittest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@DisplayName("Test of CsvSource Annotation in @ParameterizedTest")
+class CsvSourceTest {
+
+    @DisplayName("Csv Source test with two Parameters")
+    @ParameterizedTest(name = "{0} is {1} years old")
+    @CsvSource({
+            "Sadra, 28",
+            "Sara,  18",
+            "Nana,  33",
+            "Nini,   6"
+    })
+    void CsvSourceTestWithTwoParameters(String name, Integer age) {
+        assertNotNull(name);
+        assertNotEquals(0,age);
+    }
+}

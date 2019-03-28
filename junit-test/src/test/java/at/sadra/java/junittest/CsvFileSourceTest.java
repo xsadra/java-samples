2cfe49a255d5a73ae0bb18476e4699b70e2065a3
Sadra <31988724+xsadra@users.noreply.github.com>
2019-03-28 16:51:54 +0100

    Test of CsvFileSource Annotation in @ParameterizedTest

---
+++ junit-test/src/test/java/at/sadra/java/junittest/CsvFileSourceTest.java
@@ -0,0 +1,22 @@
+package at.sadra.java.junittest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@DisplayName("Test of CsvFileSource Annotation in @ParameterizedTest")
+class CsvFileSourceTest {
+
+    @DisplayName("Csv File Source test with two Parameters")
+    @ParameterizedTest(name = "{0} is {1} years old")
+    @CsvFileSource(resources = "/persons.csv")
+    void CsvFileSourceTestWithTwoParameters(String name, Integer age) {
+        assertNotNull(name);
+        assertNotEquals(0, age);
+    }
+}
